813aeff81542cbfc19a14cf1e48acc1c48c2dafa
“willy <willykaria777@gmail.com>
2026-06-02 22:07:37 +0300

    added a auth layout to

---
+++ frontend/layouts/auth.vue
@@ -0,0 +1,20 @@
+<!-- frontend/layouts/auth.vue -->
+<template>
+  <div class="auth-layout">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+.auth-layout {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
