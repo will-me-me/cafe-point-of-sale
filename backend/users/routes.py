7ba20e28878b0b961191d727dd71c500305169e1
“willy <willykaria777@gmail.com>
2026-06-08 17:02:52 +0300

    added new endpoints delete user, status updated and added user status in the userresponse

--- backend/users/routes.py
+++ backend/users/routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import  List, Optional
 from auth.jwt_handler import get_current_user
 import jwt
 from pydantic import BaseModel, EmailStr, Field, validator
 import bcrypt
 from users.schema import TokenResponse, UserCreate, UserLogin, UserResponse
-from users.services import create_user, user_login
+from users.services import activate_deactivate_user, create_user, delete_user, get_activity_logs, get_all_user, user_login
 
 router = APIRouter(prefix="/auth", tags=["authentication"])
 
@@ -18,6 +18,7 @@ async def get_me(current_user: dict = Depends(get_current_user)):
         name=current_user["name"],
         email=current_user["email"],
         role=current_user["role"],
+        status=current_user.get("status"),
         created_at=current_user["created_at"]
     )
 
@@ -28,4 +29,20 @@ async def register(user_data: UserCreate):
 @router.post("/login", response_model=TokenResponse)
 async def login(credentials: UserLogin):
     return await user_login(credentials)
+
+@router.get("/users", response_model=List[UserResponse])
+async def all_user(current_user: dict = Depends(get_current_user)):
+    return await get_all_user()
+
+@router.get("/activity-logs")
+async def get_allactivity_logs(current_user: dict = Depends(get_current_user)):
+    return await get_activity_logs()
+
+@router.put("/users/{user_id}/status")
+async def update_user_status(user_id: str, status: str, current_user: dict = Depends(get_current_user)):
+    return await activate_deactivate_user(user_id, status, current_user)
+
+@router.delete("/users/{user_id}")
+async def delete_user_by_id(user_id:str, current_user: dict = Depends(get_current_user)):
+    return await delete_user(user_id, current_user)
 
