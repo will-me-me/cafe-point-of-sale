054ca77cd2ff07a98687e0b1afcecc192fb4d4cf
“willy <willykaria777@gmail.com>
2026-06-17 10:55:07 +0300

    route to facilitate debt clearance

--- backend/orders/routes.py
+++ backend/orders/routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from typing import List
+from auth.jwt_handler import get_current_user
 from orders.schema import Order, OrderCreate, OrderItem
 from datetime import datetime
 from bson import ObjectId
@@ -22,5 +23,13 @@ async def get_orders_route():
     try:
         orders = await order_service.get_orders()
         return orders
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.put("/{order_id}/debt", response_model=Order)
+async def update_debt_order_route(order_id: str, payment_status: str,current_user: dict = Depends(get_current_user)):
+    try:
+        updated_order = await order_service.update_debt_order(order_id, payment_status,current_user)
+        return updated_order
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
